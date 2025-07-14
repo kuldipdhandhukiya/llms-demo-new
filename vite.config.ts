87bc2d86c68f4e0e51db997b1e94e3dedf9c6afa
Kuldip <kuldeep@rocktechnolabs.com>
2025-07-15 02:14:11 +0530

    fix: add plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import json from '@rollup/plugin-json';
+// ...
+plugins: [
+  // ...other plugins
+  json(),
+]
 installGlobals({ nativeFetch: true });
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
